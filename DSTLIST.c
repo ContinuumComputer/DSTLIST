d32842a9b99fa6a4c6f8d275b276e22310d9dfaf
Aldo <arivera@continuum-ct.com>
2014-10-28 13:11:49 +0100

    Added ZSYSC to include path

--- DSTLIST.c
+++ DSTLIST.c
@@ -2,40 +2,26 @@
  * DSTLIST.c
  *
  *  Created on: 15/10/2014
- *      Author: Continuum Computer Technologies
+ *  Author: Aldo Rivera, Continuum Computer Technologies
+ *
+ *  Brief:
+ * 		Prints out plain info (GMT times and offsets in seconds),
+ * 		about current loaded DST table in a NonStop Server.
+ *
+ *  Compile Env:
+ *      NSDEE 4 / H06.23 compiler env
  *
  */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#include <tal.h>
-#include <cextdecs.h>
+#include <cextdecs.h(DST_GETINFO_, INTERPRETTIMESTAMP)>
+#include <zsysdefs/ZSYSC>  // must be donwloaded from NSK
 
-#include <tnsint.h>
 #include "DSTLIST.h"
 
-#define ZSYS_VAL_DST_VERSION_SEP1997    1
-#define ZSYS_VAL_DST_OK                 0
-#define ZSYS_VAL_DST_SECURITY_ERROR     1
-#define ZSYS_VAL_DST_BAD_VERSION        2
-#define ZSYS_VAL_DST_BAD_PARAMETER      3
-#define ZSYS_VAL_DST_INTERVAL_ERROR     4
-#define ZSYS_VAL_DST_DELETE_NOW_ERROR   5
-#define ZSYS_VAL_DST_TYPE_ERROR         6
-#define ZSYS_VAL_DST_TABLE_EMPTY        7
-#define ZSYS_VAL_DST_BOUNDS_ERROR       8
-#define ZSYS_VAL_DST_RANGE_LOW          9
-#define ZSYS_VAL_DST_RANGE_HIGH         10
-#define ZSYS_VAL_DST_COUNT_OVERFLOW     11
-#define ZSYS_VAL_DST_INTERNAL_ERROR     12
-
-
-#define BOOL int
-#define TRUE -1
-#define FALSE 0
-
-char* months[] = { "?0!", "JAN", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", NULL };
+char* months[] = { "???", "JAN", "FEB", "MAR", "ABR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC", NULL };
 
 int main ( int arc, char **argv )
 {
@@ -44,8 +30,8 @@ int main ( int arc, char **argv )
 
   zsys_ddl_dst_entry_def dstEntry;
 
-  printf("RATSync/DSTLIST - DST listing program - TxxxxJ06 - (15OCT2014)\n");
-  printf("(C) COPYRIGHT 2014 - Continuum Computer Technologies, S.L.\n\n");
+  printf("RATSync/DSTLIST - DST listing program for NonStop Servers - T0000J06 - (15OCT2014)\n");
+  printf("Copyright 2014 (c) Continuum Computer Technologies, S.L.\n\n");
 
   dstEntry.z_version = ZSYS_VAL_DST_VERSION_SEP1997;
 
@@ -61,19 +47,19 @@ int main ( int arc, char **argv )
 
           INTERPRETTIMESTAMP(dstEntry.z_lowgmt, gmt);
 
-          printf ("TRANSITION %.2d %s %.4d, %.2d:%.2d GMT, ",
-              gmt[2], months[gmt[1]], gmt[0], gmt[3], gmt[4]);
+          printf ("TRANSITION %.2d %s %.4d, %.2d:%.2d GMT, "
+        		 , gmt[2], months[gmt[1]], gmt[0], gmt[3], gmt[4]);
 
           INTERPRETTIMESTAMP(dstEntry.z_highgmt, gmt);
 
-          printf ("%.2d %s %.4d, %.2d:%.2d GMT, LST + %d secs\n",
-              gmt[2], months[gmt[1]], gmt[0], gmt[3], gmt[4], dstEntry.z_offset);
+          printf ("%.2d %s %.4d, %.2d:%.2d GMT, GMT + %d secs\n"
+        		 , gmt[2], months[gmt[1]], gmt[0], gmt[3], gmt[4], dstEntry.z_offset);
        }
   }
 
   if (error != ZSYS_VAL_DST_RANGE_HIGH)
   {
-      printf("result = %d\n", error);
+      printf("*** ERROR result = %d\n", error);
   }
 
   return 0;
--- DSTLIST.h
+++ DSTLIST.h
@@ -1,19 +1,10 @@
-/* DSTTOOL.h
+/* DSTLIST.h
  *
  *
  */
-#ifndef DSTTOOL_H
-#define DSTTOOL_H
+#ifndef DSTLIST_H
+#define DSTLIST_H
 
+/* nothing to publish .. */
 
-#pragma fieldalign shared2 __zsys_ddl_dst_entry
-typedef struct __zsys_ddl_dst_entry
-{
-   long long                       z_lowgmt;
-   long long                       z_highgmt;
-   short                           z_offset;
-   short                           z_version;
-   __int32_t                       z_filler;
-} zsys_ddl_dst_entry_def;
-
-#endif /* DSTTOOL_H */
+#endif /* DSTLIST_H */
